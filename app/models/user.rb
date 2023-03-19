d3a9ee1385f4e3649327795a3d519f1187fa8704
EC2 Default User <ec2-user@ip-172-31-14-247.ap-northeast-1.compute.internal>
2023-03-19 10:27:32 +0000

    [start]kadai2-3

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
-
   has_one_attached :profile_image
-
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 },  presence: true, allow_blank: true
+  has_many :favorites, dependent: :destroy
 
 
   def get_profile_image
