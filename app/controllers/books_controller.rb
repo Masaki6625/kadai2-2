dad8e708913b3b4f027122debb0ad3fe2abaa085
EC2 Default User <ec2-user@ip-172-31-14-247.ap-northeast-1.compute.internal>
2023-03-19 02:31:51 +0000

    keika

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,10 +2,14 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
+    @books = Book.new
   end
 
   def index
     @books = Book.all
+    @book = Book.new
+    @user = current_user
   end
 
   def create
@@ -15,6 +19,7 @@ class BooksController < ApplicationController
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
+      @user = current_user
       render 'index'
     end
   end
@@ -32,15 +37,15 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :ensure_correct_user, only: [:update]
 
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
@@ -17,8 +18,9 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(current_user.id), notice: "You have updated user successfully."
     else
       render :edit
     end
