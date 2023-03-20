808f9f8e80003dec25e09f63cc1bea7c7d5f4a62
EC2 Default User <ec2-user@ip-172-31-14-247.ap-northeast-1.compute.internal>
2023-03-20 12:45:49 +0000

    keika

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,26 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :following_user, through: :follower, source: :followed
+
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :follower_user, through: :followed, source: :follower
+
+  def follow(other_user)
+    follower.create(followed_id: user_id)
+  end
+
+  def unfollow(other_user)
+    follower.find_by(followed_id: user.id).destroy
+  end
+
+  def following?(other_user)
+    following_user.include?(user)
+  end
+
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+
 end
